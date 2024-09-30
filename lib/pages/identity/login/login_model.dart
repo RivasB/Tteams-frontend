967a550f23586266a007100f15cf337b24375d05
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-30 02:09:15 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/identity/login/login_model.dart
+++ lib/pages/identity/login/login_model.dart
@@ -6,21 +6,44 @@ import 'package:flutter/material.dart';
 class LoginModel extends FlutterFlowModel<LoginWidget> {
   ///  State fields for stateful widgets in this page.
 
+  final formKey = GlobalKey<FormState>();
   // State field(s) for emailAddress widget.
   FocusNode? emailAddressFocusNode;
   TextEditingController? emailAddressTextController;
   String? Function(BuildContext, String?)? emailAddressTextControllerValidator;
+  String? _emailAddressTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Introduzca su correo electrónico';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'El formato del correo electrónico no es válido. Por favor, introduce una dirección de correo válida (ejemplo@dominio.com)';
+    }
+    return null;
+  }
+
   // State field(s) for password widget.
   FocusNode? passwordFocusNode;
   TextEditingController? passwordTextController;
   late bool passwordVisibility;
   String? Function(BuildContext, String?)? passwordTextControllerValidator;
+  String? _passwordTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Introduzca su contraseña';
+    }
+
+    return null;
+  }
+
   // Stores action output result for [Backend Call - API (login)] action in Button widget.
   ApiCallResponse? logInResponse;
 
   @override
   void initState(BuildContext context) {
+    emailAddressTextControllerValidator = _emailAddressTextControllerValidator;
     passwordVisibility = false;
+    passwordTextControllerValidator = _passwordTextControllerValidator;
   }
 
   @override
