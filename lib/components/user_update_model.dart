89c3714e5a3222acd2b485328a5b8d32f192c087
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-01 02:29:07 +0000

    Updating to latest FlutterFlow output.

--- lib/components/user_update_model.dart
+++ lib/components/user_update_model.dart
@@ -1,38 +1,12 @@
-import '/backend/api_requests/api_calls.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/form_field_controller.dart';
 import 'user_update_widget.dart' show UserUpdateWidget;
 import 'package:flutter/material.dart';
 
 class UserUpdateModel extends FlutterFlowModel<UserUpdateWidget> {
-  ///  Local state fields for this component.
-
-  List<String> organizationsIds = [];
-  void addToOrganizationsIds(String item) => organizationsIds.add(item);
-  void removeFromOrganizationsIds(String item) => organizationsIds.remove(item);
-  void removeAtIndexFromOrganizationsIds(int index) =>
-      organizationsIds.removeAt(index);
-  void insertAtIndexInOrganizationsIds(int index, String item) =>
-      organizationsIds.insert(index, item);
-  void updateOrganizationsIdsAtIndex(int index, Function(String) updateFn) =>
-      organizationsIds[index] = updateFn(organizationsIds[index]);
-
-  List<String> organizationsNames = [];
-  void addToOrganizationsNames(String item) => organizationsNames.add(item);
-  void removeFromOrganizationsNames(String item) =>
-      organizationsNames.remove(item);
-  void removeAtIndexFromOrganizationsNames(int index) =>
-      organizationsNames.removeAt(index);
-  void insertAtIndexInOrganizationsNames(int index, String item) =>
-      organizationsNames.insert(index, item);
-  void updateOrganizationsNamesAtIndex(int index, Function(String) updateFn) =>
-      organizationsNames[index] = updateFn(organizationsNames[index]);
-
   ///  State fields for stateful widgets in this component.
 
   final formKey = GlobalKey<FormState>();
-  // Stores action output result for [Backend Call - API (getAllOrganizations)] action in userUpdate widget.
-  ApiCallResponse? apiResult90x;
   // State field(s) for name widget.
   FocusNode? nameFocusNode;
   TextEditingController? nameTextController;
